24727b7f642553bd103f11e874da64c554f3edec
布莫ByGalxy <105402272+ByGalxy@users.noreply.github.com>
2024-12-25 21:36:06 +0800

    Update main.py
    1. 补充注释
    2. 学会了添加按钮

--- window system/main.py
+++ window system/main.py
@@ -8,6 +8,11 @@ def ask() -> None:
         root.destroy()
 
 
+# 0.0.2 创建并绑定按钮; 学会了创建按钮2333
+def setup_settings_click():
+    print("23333啊啊啊，我居然被点击了啊！！！")
+
+
 size = 550, 300
 root = tkt.Tk(title="linecap-doodle", size=size)
 root.center()
@@ -20,7 +25,8 @@ root.shutdown(ask)
 cv = tkt.Canvas(root, zoom_item=True)
 cv.place(width=1280, height=720)
 
-tkt.IconButton(cv, (1, 1), text="设置", image=tkt.PhotoImage(file="img/1001.png").resize(32, 32))
+tkt.IconButton(cv, (1, 1), text="设置", image=tkt.PhotoImage(file="img/1001.png").resize(32, 32),
+               command=setup_settings_click)
 root.attributes('-alpha', 0.8)
 
 root.mainloop()
